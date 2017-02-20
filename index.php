9f48a52b1ad130a2aefd3c522c0d261d5c5c0fad
ashkanapril <ashkanapril@yahoo.com>
2017-02-20 19:55:36 +1100

    first commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php include 'head.html'; ?>
+
+<div id="carousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators
+  <ol class="carousel-indicators">
+    <li data-target="#carousel" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel" data-slide-to="1"></li>
+    <li data-target="#carousel" data-slide-to="2"></li>
+    <li data-target="#carousel" data-slide-to="3"></li>
+  </ol>-->
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+
+    <div class="item active" max-height=300px>
+      <a href="https://www.facebook.com/events/673024879535690/" target="_blank"><img max-height=300px src="/images/Oweek17.jpg" class="img-responsive"></a>
+    </div>
+
+    <div class="item" max-height=300px>
+			<a href="/connect"><img max-height=300px src="/images/get_connect.png" class="img-responsive"></a>
+    </div>
+
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<hr>
+<div class="row">
+	<div class="col-sm-4">
+	<h3>WHO ARE WE?</h3>
+	<p>
+	Melbourne University Christian Union is a place for everybody to find out more about Jesus.
+	<p>
+	Absolutely everyone is welcome, it doesn't matter where you come from or what you believe in!
+	<p>
+	<a href="/jesus/" class="btn btn-primary hidden-lg hidden-md hidden-sm" role="button"><strong>More about Jesus </strong></a>
+	
+	</div>
+	<div class="col-sm-4">
+	<h3>WHAT DO WE DO?</h3>
+	<p>
+		We are a club filled with all sorts of unique people! We aim to <strong>reach out</strong> to share
+		Jesus with those around us, <strong>build up</strong> and equip believers to be Christs ambassadors and to
+		<strong>send</strong> them <strong>out</strong> to live out their faith.
+	<p>
+		<a href="/events/" class="btn btn-primary hidden-lg hidden-md hidden-sm" role="button"><strong>What's coming up</strong></a>
+	</div>
+	<div class="col-sm-4">
+	<h3>HOW DO I JOIN?</h3>
+	<p>
+		Come explore the Christian faith with us and form awesome friendships along the way!
+	<p>
+		No catches, we'd just like to get to know you!
+	<p>
+		<a href="/connect/" class="btn btn-primary hidden-lg hidden-md hidden-sm" role="button"><strong>Get connected </strong></a>
+	</div>
+</div>
+<div class="row">
+	<div class="col-sm-4 hidden-xs">
+		<a href="/jesus/" class="btn btn-primary" role="button"><strong>More about Jesus </strong></a>
+	</div>
+	<div class="col-sm-4 hidden-xs">
+		<a href="/events/" class="btn btn-primary" role="button"><strong>What's coming up</strong></a>
+	</div>
+	<div class="col-sm-4 hidden-xs">
+		<a href="/connect/" class="btn btn-primary" role="button"><strong>Get connected </strong></a>
+	</div>
+</div>
+		</div>
+	</body>
+</html>
+
+
